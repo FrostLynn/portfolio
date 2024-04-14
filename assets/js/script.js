0b7cf1dcec8ff6741e5d30cc42e2c36ae160c229
Akhdan Rafif Nugraha <akhdannugraha1@gmail.com>
2024-04-14 16:53:38 +0700

    Add Form Validation using Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,57 @@ function showSidebar(){
     sidebar.style.display = 'none'
   }
 
-  
+  $(document).ready(function() {
+    $("#contactForm").validate({
+        rules: {
+            fullname: {
+                required: true,
+                lettersOnly: true, // Menerapkan aturan lettersOnly
+                maxlength: 20
+            },
+            email: {
+                required: true,
+                email: true,
+                maxlength: 30
+            },
+            phone: {
+                required: true,
+                number: true,
+                minlength: 10,
+                maxlength: 13
+            },
+            message: {
+                required: true,
+                maxlength: 50
+            }
+        },
+        messages: {
+            fullname: {
+                required: "Nama lengkap harus diisi",
+                lettersOnly: "Nama lengkap harus berupa huruf",
+                maxlength: "Nama lengkap tidak boleh lebih dari 20 karakter"
+            },
+            email: {
+                required: "Email harus diisi",
+                email: "Format email tidak valid",
+                maxlength: "Email tidak boleh lebih dari 30 karakter"
+            },
+            phone: {
+                required: "Nomor handphone harus diisi",
+                number: "Nomor handphone harus berupa angka",
+                minlength: "Nomor handphone tidak boleh kurang dari 10 digit",
+                maxlength: "Nomor handphone tidak boleh lebih dari 12 digit"
+            },
+            message: {
+                required: "Pesan harus diisi",
+                maxlength: "Pesan tidak boleh lebih dari 50 karakter"
+            }
+        }
+    });
+
+    // Menambahkan metode kustom untuk aturan lettersOnly
+    $.validator.addMethod("lettersOnly", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z\s]*$/.test(value);
+    }, "Harus berupa huruf");
+    
+});
